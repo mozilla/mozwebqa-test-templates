8a5b5677e1a7e172c8e40971b893fa202b53183f
zacc <zcampbell@mozilla.com>
2012-08-01 15:45:22 +0100

    Update from pull comments

--- tests/test_nondestructive.py
+++ tests/test_nondestructive.py
@@ -9,22 +9,22 @@ from unittestzero import Assert
 from pages.page_object import MySiteHomePage
 
 
-class TestDestructive():
+class TestNonDestructive():
 
     @pytest.mark.nondestructive
     def test_load_baseurl_nondestructive(self, mozwebqa):
-        '''
+        """
         This test is nondestructive - it does not write
         to the database or leave a mark on the website
-        '''
+        """
         home_page = MySiteHomePage(mozwebqa)
         home_page.go_to_home_page()
         Assert.true(home_page.is_the_current_page)
 
     def test_load_baseurl_destructive(self, mozwebqa):
-        '''
+        """
         This test is *not* marked as nondestructive and
         will be automatically skipped when run against
         a sensitive (ie Production) URL
-        '''
+        """
         pass
